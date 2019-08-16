348b93d0b834e521f8e9387687543a83d1bebaba
Maverick <maverick@localhost.localdomain>
2019-08-16 16:01:59 +1000

    now checking html files

--- test/test-dgen.py
+++ test/test-dgen.py
@@ -9,6 +9,15 @@ from dgen_model import dgen_project
 
 class BuildTest(unittest.TestCase):
 
+    # Print functions
+    def logTestStart(self, name):
+        print ('-'*100)
+        print ('Testing: ' + name)
+        print ('-'*100)
+
+    def logTestEnd(self, result):
+        print ('\nTest Passed' if result else 'Test Failed')
+
     def checkFile(self, filepath):
         exists = os.path.exists(filepath)
         stdout = '%s...%r' % (filepath, exists)
@@ -19,26 +28,39 @@ class BuildTest(unittest.TestCase):
     # Generate a dgen document
     @mock.patch('sys.argv', ['../dgen/dgen.py'])
     def test_generate_document(self):
+        self.logTestStart('Document Generation')
         global document
         document = dgen()
-        self.assertTrue(True)
+        self.logTestEnd(document is not None)
     
     # Test files have been created
-    def test_template_files(self):
-        print ('\nChecking template files exists...')
-        results = []
+    def test_generated_files(self):
+        self.logTestStart('Generated Files')
+        
+        html_dir = document.project.filename + '-html'
+        
+        results = [
+            self.checkFile(html_dir),
+            self.checkFile(html_dir + '/css'),
+            self.checkFile(html_dir + '/document_information.html'),
+            self.checkFile(html_dir + '/footer.html'),
+            self.checkFile(html_dir + '/header.html'),
+            self.checkFile(html_dir + '/report.html'),
+            self.checkFile(html_dir + '/title_page.html'),
+            self.checkFile(html_dir + '/toc.template.xsl'),
+            self.checkFile(document.project.filename + '.pdf')
+        ]
 
-        # Template files - Match generated files against template source
-        for root, dirs, files in os.walk(document.project.template):
-            for fil in files:
-                results.append(self.checkFile((root + '/' + fil).replace(document.project.template, 'default')))
-
-        # Check for HTML dir, and report.pdf
-        print('Checking report files exist...')
-        results.append(self.checkFile(document.project.filename + '-html'))
-        results.append(self.checkFile(document.project.filename + '.pdf'))
-
+        self.logTestEnd((results.count(False) == 0))
         self.assertTrue((results.count(False) == 0))
+
+    # Test report PDF file attributes
+    def test_report_file(self):
+        self.logTestStart('Report PDF file size')
+        report = document.project.filename + '.pdf'
+        filesize = os.stat(report).st_size
+        self.logTestEnd((filesize > 100))
+        self.assertTrue(filesize > 100)
 
 if __name__ == '__main__':
     unittest.main()
